020d8358c62a4756ba2aa5559e1a7c5cc9a1d088
Tangsisi <23302010084@m.fudan.edu.cn>
2025-05-25 01:47:45 +0800

    Initial backend commit

---
+++ src/test/java/org/example/vehicle/VehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
